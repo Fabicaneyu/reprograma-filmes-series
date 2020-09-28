a1156573fac517d7def4a1f22ac5e1108cfc0790
Fabi <fabicaneyu@gmail.com>
2020-09-27 22:47:32 -0300

    atualizada porta para o heroku

--- reprogramaflix/server.js
+++ reprogramaflix/server.js
@@ -1,8 +1,8 @@
 const app = require("./src/app")
 
-const PORT = 8080 
+const port = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
-    console.log("Menina, o nosso app ta de pe! na porta "+ PORT)
+    console.log("Menina, o nosso app ta de pe! na porta "+ port)
 })
 
